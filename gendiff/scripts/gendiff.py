e32856ddd5651575b36a5d776c76e200d86fead3
aldangold <meloshnikoff@gmail.com>
2020-06-04 23:25:14 +0300

    step 1:help output, added main script

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python3
+
+"""The main script."""
+
+from gendiff.parser import func
+
+
+def main():
+    """Run the main code."""
+    func()
+
+
+if __name__ == '__main__':
+    main()
